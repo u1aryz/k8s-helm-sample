1b5bfd0dd07529c34ddbab1acfec1d5a4765f50e
u1aryz <u1aryz.d@gmail.com>
2021-11-24 01:56:45 +0900

    use redis on api

--- api/go.mod
+++ api/go.mod
@@ -2,13 +2,18 @@ module api
 
 go 1.17
 
-require github.com/labstack/echo v3.3.10+incompatible
+require (
+	github.com/go-redis/redis v6.15.9+incompatible
+	github.com/labstack/echo v3.3.10+incompatible
+)
 
 require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
 	github.com/labstack/gommon v0.3.1 // indirect
 	github.com/mattn/go-colorable v0.1.11 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/onsi/ginkgo v1.16.5 // indirect
+	github.com/onsi/gomega v1.17.0 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.1 // indirect
 	golang.org/x/crypto v0.0.0-20211117183948-ae814b36b871 // indirect
--- api/main.go
+++ api/main.go
@@ -1,18 +1,60 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"os"
+	"time"
 
+	"github.com/go-redis/redis"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
 )
 
+type result struct {
+	Message string `json:"message"`
+}
+
 func main() {
 	e := echo.New()
 	e.Use(middleware.Logger())
-	api := e.Group("/api")
-	api.GET("", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello")
+
+	client := redis.NewClient(&redis.Options{
+		Addr:     os.Getenv("REDIS_URL"),
+		Password: os.Getenv("REDIS_PASSWORD"),
+		DB:       0,
 	})
+
+	api := e.Group("/api")
+	api.GET("/your-name", func(c echo.Context) error {
+		val, err := client.Get("name").Result()
+
+		if err == redis.Nil {
+			return echo.NewHTTPError(http.StatusNotFound, "What's your name?")
+		}
+		if err != nil {
+			return err
+		}
+		return c.JSON(http.StatusOK, &result{
+			Message: fmt.Sprintf("Hello %s", val),
+		})
+	})
+
+	api.POST("/your-name", func(c echo.Context) error {
+		value := c.FormValue("name")
+		if value == "" {
+			return echo.NewHTTPError(http.StatusBadRequest, "You must 'name' form data")
+		}
+
+		err := client.Set("name", value, 1*time.Minute).Err()
+
+		if err != nil {
+			return err
+		}
+		return c.JSON(http.StatusOK, &result{
+			Message: fmt.Sprintf("Hello %s", value),
+		})
+	})
+
 	e.Logger.Fatal(e.Start(":8080"))
 }
